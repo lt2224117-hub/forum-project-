50ca6e6819710a3a8b1ebb75f18eb2b1a4fe7752
Luong <luong@gmail.com>
2026-05-17 10:07:11 +0700

    promote user to admin

--- build.sh
+++ build.sh
@@ -3,3 +3,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
+python manage.py shell -c "
+from django.contrib.auth.models import User
+u = User.objects.filter(username='Admin').first()
+if u:
+    u.is_staff = True
+    u.is_superuser = True
+    u.save()
+    print('Done')
+"
