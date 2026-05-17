560b2a372f8b4b523239118e1e35f0cfaa50d933
Luong <luong@gmail.com>
2026-05-17 10:35:57 +0700

    remove make admin view

--- config/urls.py
+++ config/urls.py
@@ -2,17 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse
-from django.contrib.auth.models import User
 
-def make_admin(request):
-    u = User.objects.filter(username='Admin').first()
-    if u:
-        u.is_staff = True
-        u.is_superuser = True
-        u.save()
-        return HttpResponse('Done! Admin promoted.')
-    return HttpResponse('User not found.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -21,5 +11,4 @@ urlpatterns = [
     path('interaction/', include('interaction.urls')),
     path('search/', include('search.urls')),
     path('moderation/', include('moderation.urls')),
-    path('make-admin/', make_admin),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
