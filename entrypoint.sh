e41c1a362a8367af83aead857e6dbb632728e7da
Luong <luong@gmail.com>
2026-05-29 10:44:12 +0700

    fix entrypoint set admin password

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ u = User.objects.filter(username='Admin').first()
 if u:
     u.is_staff = True
     u.is_superuser = True
+    u.set_password('Admin123456')
     u.save()
-    print('Promoted to admin')
+    print('Done')
 "
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
