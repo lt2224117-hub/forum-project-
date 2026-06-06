9adc0e5dd97023043860937256771d81226e96d1
Luong <luong@gmail.com>
2026-06-06 17:34:58 +0700

    fix: url vote nhan so am

--- interaction/urls.py
+++ interaction/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 app_name = 'interaction'
 
 urlpatterns = [
     path('like/<int:post_id>/', views.like_post, name='like_post'),
-    path('vote/<int:topic_id>/<int:value>/', views.vote_topic, name='vote_topic'),
+    re_path(r'^vote/(?P<topic_id>\d+)/(?P<value>-?\d+)/$', views.vote_topic, name='vote_topic'),
 ]
